4b0d0d2f333d7b4ec194a51162609438866d5a7e
Alexander labrador <alexlador2@gmail.com>
2023-10-21 13:28:30 +0200

    fix(dependencies): remove extra dependencies

--- chat/config.js
+++
@@ -1,9 +0,0 @@
-import { Configuration, OpenAIApi } from "openai";
-require("dotenv").config();
-
-const configuration = new Configuration({
-  organization: process.env.OPENAI_ORGANIZATION_ID,
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-export const OpenAI = new OpenAIApi(configuration);
--- routes/chat.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const { postMessage } = require("../controllers/chat");
-
-const router = Router();
-
-router.get("/", postMessage);
-
-module.exports = router;
